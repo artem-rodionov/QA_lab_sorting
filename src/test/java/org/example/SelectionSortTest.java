bd6767fbba96a571aa266d402bed9d9124266618
hatka_bobrikov <rodionov.artem.for.docs@gmail.ru>
2024-04-06 00:52:24 +0300

    New name for test methods

--- src/test/java/org/example/SelectionSortTest.java
+++ src/test/java/org/example/SelectionSortTest.java
@@ -10,35 +10,35 @@ import org.junit.Test;
 public class SelectionSortTest {
 
     @Test
-    public void selectionSort1() {
+    public void selectionSortGoodArray() {
         int []arr = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         SelectionSort.selectionSort(arr);
         Assert.assertArrayEquals(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}, arr);
     }
 
     @Test
-    public void selectionSort2() {
+    public void selectionSortEmptyArray() {
         int []arr = {};
         SelectionSort.selectionSort(arr);
         Assert.assertArrayEquals(new int[]{}, arr);
     }
 
     @Test
-    public void selectionSort3() {
+    public void selectionSortOneElementArray() {
         int []arr = {1};
         SelectionSort.selectionSort(arr);
         Assert.assertArrayEquals(new int[]{1}, arr);
     }
 
     @Test
-    public void selectionSort4() {
+    public void selectionSortSameElementsArray() {
         int []arr = {1, 1, 1, 1, 1};
         SelectionSort.selectionSort(arr);
         Assert.assertArrayEquals(new int[]{1, 1, 1, 1, 1}, arr);
     }
 
     @Test
-    public void selectionSort5() {
+    public void selectionSortRandomElementsArray() {
         int []arr = {22, 35, 76, 49, 19, 13, 72, 34, 26, 53, 8, 59, 73, 90, 48, 82, 30, 15, 83, 6};
         SelectionSort.selectionSort(arr);
         Assert.assertArrayEquals(new int[]{6, 8, 13, 15, 19, 22, 26, 30, 34, 35, 48, 49, 53, 59, 72, 73, 76, 82, 83, 90}, arr);
